e7f89ea60fff8b01166d37e0a99a8f0df7910df6
Jan König <jankoenig120190@googlemail.com>
2020-04-04 09:42:33 +0200

    🔧 Update Staging

--- project.js
+++ project.js
@@ -3,11 +3,55 @@
 // ------------------------------------------------------------------
 
 module.exports = {
+    /*
+        PLATFORMS: Where to deploy the project
+        Learn more > jovo.tech/docs/platforms
+    */
     alexaSkill: {
         nlu: 'alexa',
     },
     googleAction: {
         nlu: 'dialogflow',
     },
-    endpoint: '${JOVO_WEBHOOK_URL}',
+
+    /*
+        STAGING: Setting up different staging environments
+        Learn more > jovo.tech/docs/staging
+    */
+    defaultStage: 'local',
+    stages: {
+        local: {
+            endpoint: '${JOVO_WEBHOOK_URL}',
+            alexaSkill: {
+                skillId: process.env.LOCAL_SKILL_ID,
+                askProfile: process.env.LOCAL_ASK_PROFILE,
+            },
+        },
+        dev: {
+            endpoint: process.env.DEV_ENDPOINT,
+            alexaSkill: {
+                skillId: process.env.DEV_SKILL_ID,
+                askProfile: process.env.DEV_ASK_PROFILE,
+            },
+        },
+        prod: {
+            endpoint: PROD_ENDPOINT,
+            alexaSkill: {
+                skillId: process.env.PROD_SKILL_ID,
+                askProfile: process.env.PROD_ASK_PROFILE,
+            },
+        },
+    },
+
+    /*
+        DEPLOYMENT: Config for the jovo deploy command
+        Learn more > jovo.tech/docs/cli/deploy
+    */
+    deploy: {
+        target: [
+        'info',
+        'model'
+        //  'zip'
+        ]
+    },
 };
